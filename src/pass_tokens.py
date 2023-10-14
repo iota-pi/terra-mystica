9b24d203a460916753f04be1120ed1918191af1a
David Adams <david.adams.16.12@gmail.com>
2023-10-14 20:45:07 +0000

    Add pass tokens

---
+++ src/pass_tokens.py
@@ -0,0 +1,44 @@
+from AbstractResources import AbstractResources
+from Building import Building
+from Resources import Resources
+from PassToken import PassToken
+
+
+ROUND_TOKENS = [
+    PassToken(
+        bonus_condition=Building.DWELLING,
+        bonus_points=1,
+        income=Resources(coins=2),
+    ),
+    PassToken(
+        bonus_condition=Building.TRADING_HOUSE,
+        bonus_points=2,
+        income=Resources(workers=1),
+    ),
+    PassToken(
+        bonus_condition=(Building.STRONGHOLD, Building.SANCTUARY),
+        bonus_points=4,
+        income=Resources(workers=2),
+    ),
+    PassToken(
+        income=Resources(workers=1, power=3),
+    ),
+    PassToken(
+        income=Resources(power=3),
+        abstract_resources=AbstractResources(shipping_bonus=1),
+    ),
+    PassToken(
+        income=Resources(priests=1),
+    ),
+    PassToken(
+        income=Resources(coins=6),
+    ),
+    PassToken(
+        income=Resources(coins=4),
+        # TODO cult action
+    ),
+    PassToken(
+        income=Resources(coins=2),
+        # TODO spade action
+    ),
+]
