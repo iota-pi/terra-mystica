594e7babbc691753a764ddffe19cb01718855483
Werdna6001 <andrew.adams1@students.mq.edu.au>
2023-10-15 22:22:26 +1100

    fixing an error

--- src/Tile.py
+++ src/Tile.py
@@ -14,17 +14,17 @@ class Tile:
             self.building = building
 
     def can_build(self, building: Building) -> bool:
-        if self.building is None and building == Building.DWELLING:
+        if self.building is None and self.building == Building.DWELLING:
             return True
         if self.building is None:
             return False
-        if self.building == Building.DWELLING and building == Building.TRADING_HOUSE:
+        if self.building == Building.DWELLING and self.building == Building.TRADING_HOUSE:
             return True
         if self.building == Building.TRADING_HOUSE:
-            if building == Building.STRONGHOLD:
+            if self.building == Building.STRONGHOLD:
                 return True
-            if building == Building.TEMPLE:
+            if self.building == Building.TEMPLE:
                 return True
-        if self.building == Building.TEMPLE and building == Building.SANCTUARY:
+        if self.building == Building.TEMPLE and self.building == Building.SANCTUARY:
             return True
         return False
