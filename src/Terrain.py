fe164f907dfc1bea6aa2b47c71246307ae380414
David Adams <david.adams.16.12@gmail.com>
2023-10-21 06:23:13 +0000

    Add method to calculate terraform spade cost

--- src/Terrain.py
+++ src/Terrain.py
@@ -11,3 +11,28 @@ class Terrain(Enum):
     DESERT = 6
     WASTELAND = 7
     RIVER = 8
+
+
+TERRAIN_ORDER = (
+    Terrain.MOUNTAIN,
+    Terrain.FOREST,
+    Terrain.LAKE,
+    Terrain.SWAMP,
+    Terrain.FIELD,
+    Terrain.DESERT,
+    Terrain.WASTELAND,
+)
+
+
+def calculate_spade_cost(current: Terrain, target: Terrain) -> int:
+    try:
+        current_position = TERRAIN_ORDER.index(current)
+    except ValueError:
+        raise ValueError(f"Invalid terrain type for terraforming: {current}")
+    try:
+        target_position = TERRAIN_ORDER.index(target)
+    except ValueError:
+        raise ValueError(f"Invalid terrain type for terraforming: {target}")
+
+    cost = abs(current_position - target_position)
+    return cost
