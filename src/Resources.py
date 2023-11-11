404bb761f18c31289ce6e77a1a601cbb1bf50aa4
David Adams <david.adams.16.12@gmail.com>
2023-11-11 02:24:59 +0000

    Implement power gaining and spending mechanic

--- src/Resources.py
+++ src/Resources.py
@@ -1,10 +1,24 @@
 from dataclasses import dataclass
+from Power import Power
 from util import ArithmeticEnabledDataclass
 
 
-@dataclass(frozen=True, slots=True)
+@dataclass(frozen=True, slots=True, init=False)
 class Resources(ArithmeticEnabledDataclass):
     workers: int = 0
-    power: int = 0
+    power: Power = Power()
     coins: int = 0
     priests: int = 0
+
+    def __init__(
+        self,
+        workers: int = 0,
+        power: int | Power = 0,
+        coins: int = 0,
+        priests: int = 0,
+    ):
+        # Work around limitations of frozen dataclasses
+        object.__setattr__(self, 'workers', workers)
+        object.__setattr__(self, 'power', Power(power))
+        object.__setattr__(self, 'coins', coins)
+        object.__setattr__(self, 'priests', priests)
